00b7a2546f605ed6589518ee777679a118f7ec5b
Daniel-Cross <crossy1686@gmail.com>
2018-09-26 11:52:16 +0100

    added profile functions

--- src/github.js
+++ src/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = 'dae3f43cef813cdc42c7';
     this.client_secret = '60be22724b3cd89717411e4764648f71587ef944';
+    this.repos_count = 6;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -11,10 +13,20 @@ class GitHub {
       }&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
--- src/ui.js
+++ src/ui.js
@@ -37,6 +37,35 @@ class UI {
     `;
   }
 
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">
+            ${repo.name}
+          </a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-secondary">Stars: ${
+              repo.stargazers_count
+            }</span>
+            <span class="badge badge-success">Watchers: ${
+              repo.watchers_count
+            }</span>
+            <span class="badge badge-info">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
   //Show alert message
   showAlert(message, className) {
     //clear
